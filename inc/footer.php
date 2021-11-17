303df5e811d10a306c2f6edd2c4230f8020f0356
maxencelgy <maxencelgy@gmail.com>
2021-11-17 14:00:46 +0100

    ae

--- inc/footer.php
+++ inc/footer.php
@@ -1,27 +1,27 @@
 
-<section id="footer">
-    <nav>
-        <ul>
-            <li><a href="#">Mention légale</a></li>
-            <li>&copy; Vacuna Del Sol</li>
-            <li><label for="mail"></label>
-                <input type="email" id="mail" name="mail" placeholder="nous contacter">
-                <span class="error"></span>
-            </li>
-            <li><a href="tel:+33658564"> Appelez nous </a></li>
-
-        </ul>
-    </nav>
-</section>
-<section id="reseaux_sociaux">
-    <div class="reseau">
-        <ul>
-            <li><a href="#"><img src="asset/img/facebook.svg" alt=""></a></li>
-            <li><a href="#"><img src="asset/img/snapchat.png" alt=""></a></li>
-            <li><a href="#"><img src="asset/img/instagram.svg" alt=""></a></li>
-        </ul>
-    </div>
-</section>
+<!--<section id="footer">-->
+<!--    <nav>-->
+<!--        <ul>-->
+<!--            <li><a href="#">Mention légale</a></li>-->
+<!--            <li>&copy; Vacuna Del Sol</li>-->
+<!--            <li><label for="mail"></label>-->
+<!--                <input type="email" id="mail" name="mail" placeholder="nous contacter">-->
+<!--                <span class="error"></span>-->
+<!--            </li>-->
+<!--            <li><a href="tel:+33658564"> Appelez nous </a></li>-->
+<!---->
+<!--        </ul>-->
+<!--    </nav>-->
+<!--</section>-->
+<!--<section id="reseaux_sociaux">-->
+<!--    <div class="reseau">-->
+<!--        <ul>-->
+<!--            <li><a href="#"><img src="asset/img/facebook.svg" alt=""></a></li>-->
+<!--            <li><a href="#"><img src="asset/img/snapchat.png" alt=""></a></li>-->
+<!--            <li><a href="#"><img src="asset/img/instagram.svg" alt=""></a></li>-->
+<!--        </ul>-->
+<!--    </div>-->
+<!--</section>-->
 
 </body>
 </html>
