cab02a6add6111c822f2e7d4414e4f4fd80d2a8f
mathisdls <mathisdllss@gmail.com>
2021-11-17 11:51:52 +0100

    dsfs

--- inc/footer.php
+++ inc/footer.php
@@ -5,12 +5,8 @@
         <ul>
             <li><a href="#">Mention légale</a></li>
             <li>&copy; Vacuna Del Sol</li>
-            <li><label for="mail"></label>
-                <input type="email" id="mail" name="mail" placeholder="nous contacter">
-                <span class="error"></span>
-            </li>
+            <li><a href="mailto:">Nous contacter</a></li>
             <li><a href="tel:+33658564"> Appelez nous </a></li>
-
         </ul>
     </nav>
     <div class="reseau">
