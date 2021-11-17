900f1f741a884ca75ffe62bb98e1df12149f386a
mathisdls <mathisdllss@gmail.com>
2021-11-17 10:06:43 +0100

    addqd

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,26 @@
-
+<section id="footer">
+<nav>
+    <ul>
+        <li><a href="#">Mention légale</a></li>
+        <li>&copy; Vacuna Del Sol</li>
+        <li><label for="mail"></label>
+            <input type="email" id="mail" name="mail" placeholder="nous contacter">
+            <span class="error"></span>
+        </li>
+        <li><a href="tel:+33658564"> Appelez nous </a></li>
+        
+    </ul>
+</nav>
+</section>
+<section id="reseaux_sociaux">
+    <div class="reseau">
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </div>
+</section>
 
 </body>
 </html>
