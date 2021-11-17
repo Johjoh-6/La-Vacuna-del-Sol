cddffaf0486464adca8f9a1000e671d0c606fece
mathisdls <mathisdllss@gmail.com>
2021-11-17 11:05:51 +0100

    sqdqd

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,6 @@
 
 <section id="footer">
+    <div class="wrap6">
     <nav>
         <ul>
             <li><a href="#">Mention légale</a></li>
@@ -14,9 +15,11 @@
     </nav>
     <div class="reseau">
         <ul>
-            <li><a href="#"><img src="asset/img/facebook.svg" alt=""></a></li>
-            <li><a href="#"><img src="asset/img/instagram.svg" alt=""></a></li>
+            <li><a href="#"><img src="asset/img/facebook-svgrepo-com (1).svg" alt=""></a></li>
+            <li><a href="#"><img src="asset/img/instagram-svgrepo-com.svg" alt=""></a></li>
+            <li><a href=""><img src="asset/img/twitter-svgrepo-com.svg" alt=""></a></li>
         </ul>
+    </div>
     </div>
 </section>
 
