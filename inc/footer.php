0d61bef59221b463ac5c9a8771a1f04342c31bf3
maxencelgy <maxencelgy@gmail.com>
2021-11-18 15:24:39 +0100

    responsiv

--- inc/footer.php
+++ inc/footer.php
@@ -1,26 +1,36 @@
 <footer id="footer">
     <div class="wrap6">
-    <nav>
-        <ul>
-            <li><a href="legal-mention.php">Mention légale</a></li>
-            <li><a href="contact.php">Nous contacter</a></li>
-            <li><a href="tel:+33658564"> Appelez nous </a></li>
-        </ul>
-    </nav>
-        <div class="copy"> <span> &copy; Vacuna Del Sol</span></div>
-    <div class="reseau">
-        <ul>
-            <li><a href="#" target="_blank"><img src="asset/img/facebook-svgrepo-com (1).svg" alt=""></a></li>
-            <li><a href="#"target="_blank"><img src="asset/img/instagram-svgrepo-com.svg" alt=""></a></li>
-            <li><a href="#" target="_blank"><img src="asset/img/twitter-svgrepo-com.svg" alt=""></a></li>
-        </ul>
-    </div>
-    </div>
-
+        <nav>
+            <ul>
+                <li><a href="legal-mention.php">Mention légale</a></li>
+                <li><a href="contact.php">Nous contacter</a></li>
+                <li><a href="tel:+33658564"> Appelez nous </a></li>
+            </ul>
+        </nav>
+        <div class="copy">&copy; Vacuna Del Sol Creator</div>
+        <div class="reseau">
+            <ul>
+                <li><a href="#" target="_blank"><img src="asset/img/facebook-svgrepo-com (1).svg" alt=""></a></li>
+                <li><a href="#"target="_blank"><img src="asset/img/instagram-svgrepo-com.svg" alt=""></a></li>
+                <li><a href="#" target="_blank"><img src="asset/img/twitter-svgrepo-com.svg" alt=""></a></li>
+            </ul>
+        </div>
+</div>
 </footer>
 
 <!--<script src="../asset/js/main.js"></script>-->
-<script>const wrapper = document.querySelector('.wrapper');
+<script>
+    function myFunction() {
+        var x = document.getElementById("navbar");
+        if (x.className === "nav") {
+            x.className += " responsive";
+        } else {
+            x.className = "nav";
+        }
+    }
+</script>
+<script>
+    const wrapper = document.querySelector('.wrapper');
     const indicators = [...document.querySelectorAll('.indicators button')];
 
     let currentTestimonial = 0; // Default 0
--- index.php
+++ index.php
@@ -10,7 +10,6 @@ include ('inc/header.php');
 
 <!--MAIN INDEX USER-->
 
-
 <section id="connexion">
     <div class="wrap2">
         <div class="box">
@@ -23,11 +22,11 @@ include ('inc/header.php');
         <div class="cadre">
             <form action="" method="post" class="wrapform" novalidate>
                 <label for="login">E-mail ou Pseudo</label>
-                <input type="text" id="login" name="login" value="" class="input" placeholder="&#xF002;  Entrez votre email ou votre pseudo " style="font-family:Arial, FontAwesome">
+                <input type="text" id="login" name="login" value="" class="input" placeholder="&#xf007;  Entrez votre email ou votre pseudo " style="font-family:Arial, FontAwesome">
                 <span class="error"></span>
 
                 <label for="password">Mot de passe *</label>
-                <input type="password" id="password" name="password" value="" class="input" placeholder="&#xF002;Entrez votre mot de passe">
+                <input type="password" id="password" name="password" value="" class="input" placeholder="&#xf023; Entrez votre mot de passe" style="font-family:Arial, FontAwesome">
                 <input type="submit" name="submitted" value="Connexion" class="submit input2">
                 <a href="inscription.php" class="submit">Inscription</a>
             </form>
@@ -91,39 +90,33 @@ include ('inc/header.php');
         <div class="container">
             <div class="wrapper">
                 <div class="card">
+                    <div class="card-body">
+                        <p class="review"><i class="fas fa-quote-left"></i> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, nam. Fugiat, ut provident. Nulla quibusdam labore ea est consequatur sequi fuga iusto laudantium, earum doloremque? <i class="fas fa-quote-right"></i></p>
+                        <div class="rating">
+                            <img src="asset/img/star-fill.png" class="star" alt="">
+                            <img src="asset/img/star-fill.png" class="star" alt="">
+                            <img src="asset/img/star-fill.png" class="star" alt="">
+                            <img src="asset/img/star-fill.png" class="star" alt="">
+                            <img src="asset/img/star-no-fill.png" class="star" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <p class="review"><i class="fas fa-quote-left"></i> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, nam. Fugiat, ut provident. Nulla quibusdam labore ea est consequatur sequi fuga iusto laudantium, earum doloremque? <i class="fas fa-quote-right"></i></p>
+                        <div class="rating">
+                            <img src="asset/img/star-fill.png" class="star" alt="">
+                            <img src="asset/img/star-fill.png" class="star" alt="">
+                            <img src="asset/img/star-fill.png" class="star" alt="">
+                            <img src="asset/img/star-fill.png" class="star" alt="">
+                            <img src="asset/img/star-no-fill.png" class="star" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
 
                     <div class="card-body">
-                        <p class="review">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, nam. Fugiat, ut provident. Nulla quibusdam labore ea est consequatur sequi fuga iusto laudantium, earum doloremque?</p>
-                        <div class="rating">
-                            <img src="asset/img/star-fill.png" class="star" alt="">
-                            <img src="asset/img/star-fill.png" class="star" alt="">
-                            <img src="asset/img/star-fill.png" class="star" alt="">
-                            <img src="asset/img/star-fill.png" class="star" alt="">
-                            <img src="asset/img/star-no-fill.png" class="star" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="card-thumb">
-                        <span class="client-name">client two</span>
-                    </div>
-                    <div class="card-body">
-                        <p class="review"><i class="fas fa-quote-left"></i>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, nam. Fugiat, ut provident. Nulla quibusdam labore ea est consequatur sequi fuga iusto laudantium, earum doloremque?<i class="fas fa-quote-right"></i></p>
-                        <div class="rating">
-                            <img src="asset/img/star-fill.png" class="star" alt="">
-                            <img src="asset/img/star-fill.png" class="star" alt="">
-                            <img src="asset/img/star-fill.png" class="star" alt="">
-                            <img src="asset/img/star-fill.png" class="star" alt="">
-                            <img src="asset/img/star-no-fill.png" class="star" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="card-thumb">
-                        <span class="client-name">client three</span>
-                    </div>
-                    <div class="card-body">
-                        <p class="review">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, nam. Fugiat, ut provident. Nulla quibusdam labore ea est consequatur sequi fuga iusto laudantium, earum doloremque?</p>
+                        <p class="review"><i class="fas fa-quote-left"></i> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, nam. Fugiat, ut provident. Nulla quibusdam labore ea est consequatur sequi fuga iusto laudantium, earum doloremque? <i class="fas fa-quote-right"></i></p>
                         <div class="rating">
                             <img src="asset/img/star-fill.png" class="star" alt="">
                             <img src="asset/img/star-fill.png" class="star" alt="">
